0700cce5169495db48e80d32737d8bc5596d2596
root <root@OMODIAGBE-DANIEL-ONOSETALE.localdomain>
2023-01-05 12:22:07 +0100

    a type-annotated function to_str that takes a float n as argument and returns the string representation of the float

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+""" a typed-annotated function to_str"""
+
+
+def to_str(n: float) -> str:
+    """a typed-annotated function to_str that takes a float n
+       as argument and return the string representation"""
+
+    return str(n)

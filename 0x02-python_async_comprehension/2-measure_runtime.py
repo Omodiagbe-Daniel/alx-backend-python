3b64d0af6adf75d13bf6c094db8878b134002ffd
root <root@OMODIAGBE-DANIEL-ONOSETALE.localdomain>
2023-01-10 16:54:53 +0100

    documentated function

--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -9,6 +9,13 @@ async_com = __import__("1-async_comprehension").async_comprehension
 
 
 async def measure_runtime() -> float:
+    """
+        Import async_comprehension from the previous file and
+        write a measure_runtime coroutine that will execute
+        async_comprehension four times in parallel using asyncio.gather
+        measure_runtime should measure the total runtime and return it
+    """
+
     start = pf()
     await (gather(async_com(), async_com(), async_com(), async_com()))
     stop = pf()

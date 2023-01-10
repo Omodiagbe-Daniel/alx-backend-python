6cfa08654823ea43b5fccea0291da5412bd58f3a
root <root@OMODIAGBE-DANIEL-ONOSETALE.localdomain>
2023-01-10 15:48:22 +0100

    annotated function

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -8,7 +8,7 @@ import typing
 async_generator = __import__("0-async_generator").async_generator
 
 
-async def async_comprehension():
+async def async_comprehension() -> List[float]:
     """
         an async function that imports async_generator
         collects 10 random numbers using async comprehension

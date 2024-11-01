ab4528a161c33aa4d3c137b253cfb9e35650e47e
Daniel-Omodiagbe <danielomodiagbe26@gmail.com>
2024-11-01 23:38:00 +0100

    a type-annotated function to_str that takes a float n as argument and returns the string representation of the float

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""type-annotated function that converts a float to a string"""
+
+
+def to_str(n: float) -> str:
+    """ a function that takes a float and returms a string representation"""
+    return str(n)

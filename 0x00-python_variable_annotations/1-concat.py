aa8d9dcc3112b16dee800792cf56b33b7023183a
Daniel-Omodiagbe <danielomodiagbe26@gmail.com>
2024-11-01 23:25:45 +0100

    returns a concatenated string

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""type-annotated function concat function"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """takes two string and return a concatenated form of them"""
+    return str1 + str2
